20bbf3fa7bbbe2efe0c0ba31796dc7f2edd98f82
Farwa Ahmad <81020141+farwa-ahmad@users.noreply.github.com>
2026-09-18 20:09:55 +0500

    chore: connect MyList to production Firebase project

---
+++ MyList/app/src/androidTest/java/com/farwaahmad/mylist/ExampleInstrumentedTest.java
@@ -0,0 +1,21 @@
+package com.farwaahmad.mylist;
+
+import android.content.Context;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    @Test
+    public void appContextUsesProductionPackageName() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.farwaahmad.mylist", appContext.getPackageName());
+    }
+}
