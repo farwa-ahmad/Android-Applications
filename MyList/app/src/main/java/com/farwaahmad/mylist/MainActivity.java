1bba3011e0d597870c36ac1d46d86295d7a0b842
Farwa Ahmad <81020141+farwa-ahmad@users.noreply.github.com>
2026-09-19 06:19:00 +0500

    fix MyList contextual hint build

--- MyList/app/src/main/java/com/farwaahmad/mylist/MainActivity.java
+++ MyList/app/src/main/java/com/farwaahmad/mylist/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.farwaahmad.mylist.adapter.TaskAdapter;
 import com.farwaahmad.mylist.data.AuthRepository;
